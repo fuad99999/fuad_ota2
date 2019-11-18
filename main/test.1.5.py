9f87a8d47d28a8a7e598c4bb4f5c9bb83f2c7df2
kitsunegeisha <fuadkassim93@gmail.com>
2019-11-18 09:09:01 +0800

    Delete test.1.5.py

--- main/test.1.5.py
+++
@@ -1,6 +0,0 @@
-import glob
-import os
-
-list_of_files = glob.glob('/path/to/folder/*') # * means all if need specific format then *.csv
-latest_file = max(list_of_files, key=os.path.getctime)
-print latest_file
